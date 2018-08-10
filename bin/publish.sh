14bcfb6fd61d63e884849c3c5f4bc5b6a4e6c203
ej9x <ivan.pinchuk@8base.com>
2018-08-10 15:49:47 +0300

    fix(build): add auth-provider package to publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -5,6 +5,7 @@ status=0
 (./bin/try-publish.sh "api-client") || status=1
 (./bin/try-publish.sh "apollo-links") || status=1
 (./bin/try-publish.sh "apollo-provider") || status=1
+(./bin/try-publish.sh "auth-provider") || status=1
 (./bin/try-publish.sh "create-apollo-client") || status=1
 (./bin/try-publish.sh "forms") || status=1
 (./bin/try-publish.sh "utils") || status=1
