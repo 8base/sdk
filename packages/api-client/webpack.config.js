728e2772e412d3ac8ac3ee3ea5f448d956de9033
zouxuoz <zouxuoz@gmail.com>
2018-08-29 12:54:18 +0300

    build(api-client): use webpack

---
+++ packages/api-client/webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+
+const LIBRARY_NAME = 'api-client';
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = `${LIBRARY_NAME}.min.js`;
+} else {
+  mode = 'development';
+  outputFile = `${LIBRARY_NAME}.js`;
+}
+
+const config = {
+  mode,
+  entry: `${__dirname}/src/index.js`,
+  devtool: 'source-map',
+  output: {
+    path: `${__dirname}/lib`,
+    filename: outputFile,
+    library: LIBRARY_NAME,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'typeof self !== \'undefined\' ? self : this',
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js'],
+  },
+};
+
+module.exports = config;
