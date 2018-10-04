3b3f0aaad97e8df1df25da7651c4f51c0a8c639e
ej9x <ivan.pinchuk@8base.com>
2018-10-04 17:44:42 +0300

    fix(utils): ignore null and undefined values in file check

--- packages/utils/src/verifiers/isFileReference.js
+++ packages/utils/src/verifiers/isFileReference.js
@@ -1,9 +1,13 @@
 //@flow
+import * as R from 'ramda';
+
 import { isFileField } from './isFileField';
 import type { FieldSchema } from '../types';
 
 const isFileReference = (fieldSchema: FieldSchema, data: any) => (
   isFileField(fieldSchema)
+  &&
+  !R.isNil(data)
   &&
   (typeof data === 'string' || data.length === 0 || typeof data[0] === 'string')
 );
