93b01e46bc29225046f58663a189d02ebf16c9cb
zouxuoz <zouxuoz@gmail.com>
2018-10-17 15:32:47 +0300

    chore(file-inpit): add to the publish task

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,6 +7,7 @@ status=0
 (./bin/try-publish.sh "app-provider") || status=1
 (./bin/try-publish.sh "auth") || status=1
 (./bin/try-publish.sh "crud") || status=1
+(./bin/try-publish.sh "file-input") || status=1
 (./bin/try-publish.sh "apollo-client") || status=1
 (./bin/try-publish.sh "forms") || status=1
 (./bin/try-publish.sh "table-schema-provider") || status=1
