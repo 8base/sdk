6f338b891a55d61db1851a1d2d1375508605c2e4
zouxuoz <zouxuoz@gmail.com>
2018-11-01 16:32:23 +0300

    fix(global): rid of hardcode externals

--- packages/utils/webpack.config.js
+++ packages/utils/webpack.config.js
@@ -2,4 +2,16 @@ const { configBuilder } = require('@8base/webpack-configuration');
 
 const LIBRARY_NAME = 'utils';
 
-module.exports = configBuilder(__dirname, LIBRARY_NAME);
+const externals = {
+  '@8base/sdk': '@8base/sdk',
+  ramda: 'ramda',
+};
+
+module.exports = configBuilder(__dirname, LIBRARY_NAME, {
+  prod: {
+    externals,
+  },
+  dev: {
+    externals,
+  },
+});
