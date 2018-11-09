0aeed82300cf067d16c1fdd82ec4f5b08d4f51f1
ej9x <ivan.pinchuk@8base.com>
2018-11-09 16:53:32 +0300

    feat(build): adds webpack for the several packages

--- packages/apollo-client/webpack.config.js
+++ packages/apollo-client/webpack.config.js
@@ -1,5 +1,5 @@
 const { configBuilder } = require('@8base/webpack-configuration');
 
-const LIBRARY_NAME = 'utils';
+const LIBRARY_NAME = 'apollo-client';
 
 module.exports = configBuilder(__dirname, LIBRARY_NAME);
