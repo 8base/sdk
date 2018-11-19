08473963d49b93763075febb8fff06d1a376e2e9
zouxuoz <zouxuoz@gmail.com>
2018-11-19 16:53:45 +0300

    fix(api-client): rid of email

--- packages/api-client/src/Client.js
+++ packages/api-client/src/Client.js
@@ -8,10 +8,9 @@ import * as R from 'ramda';
 import { importWorkspace } from './importWorkspace';
 
 const USER_REFRESH_TOKEN_QUERY = gql`
-  mutation UserRefreshToken($refreshToken: String!, $email: String!) {
+  mutation UserRefreshToken($refreshToken: String!) {
     userRefreshToken(data: {
       refreshToken: $refreshToken,
-      email: $email,
     }) {
       refreshToken
       idToken
@@ -56,7 +55,6 @@ class Client {
   gqlc: GraphQLClient;
 
   workspaceId: string;
-  email: string;
   idToken: ?string;
   refreshToken: string;
 
@@ -87,14 +85,6 @@ class Client {
   }
 
   /**
-   * Update user email.
-   * @param email - The user email.
-   */
-  setEmail(email: string) {
-    this.email = email;
-  }
-
-  /**
    * Update workspace identifier.
    * @param workspaceId - The workspace identifier.
    */
@@ -104,7 +94,7 @@ class Client {
   }
 
   async tryToRefreshToken(err: GraphQLClientError) {
-    const { refreshToken, email } = this;
+    const { refreshToken } = this;
 
     this.setIdToken(null);
 
@@ -113,7 +103,6 @@ class Client {
     try {
       response = await this.gqlc.request(USER_REFRESH_TOKEN_QUERY, {
         refreshToken,
-        email,
       });
     } catch (err) {
       throw new RefreshTokenInvalidError();
