b587071d8bf3e228d5b8cc83cf8d0d772f5b6ea0
ej9x <strcmp9@gmail.com>
2019-01-23 19:47:03 +0300

    build: move packages build to pure babel

--- packages/crud/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const { configBuilder } = require('@8base/webpack-configuration');
-
-const LIBRARY_NAME = 'crud';
-
-module.exports = configBuilder(__dirname, LIBRARY_NAME);
--- packages/webpack-configuration/index.js
+++
@@ -1,9 +0,0 @@
-const configBuilder = require('./configBuilder.js');
-const devBuilder = require('./devBuilder.js');
-const prodBuilder = require('./prodBuilder.js');
-
-module.exports = {
-  configBuilder,
-  devBuilder,
-  prodBuilder,
-};
