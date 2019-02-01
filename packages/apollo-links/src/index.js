0975b2ecd67c60c2c09f275f16cb47fb8a9baba9
ej9x <strcmp9@gmail.com>
2019-02-01 15:44:41 +0300

    feat(apollo-client): add feature to extends link

--- packages/apollo-links/src/index.js
+++ packages/apollo-links/src/index.js
@@ -4,6 +4,5 @@ export { TokenRefreshLink } from './TokenRefreshLink';
 export { AuthLink } from './AuthLink';
 export { AuthHeadersLink } from './AuthHeadersLink';
 export { SuccessLink } from './SuccessLink';
-export { SubscriptionClientLink as SubscriptionLink } from '@8base/sdk';
 export * from './types';
 
