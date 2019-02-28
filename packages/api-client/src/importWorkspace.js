12f4cad7d893f993b3d5bd5cdb62a75da94de106
Osipov Vladimir <zouxuoz@gmail.com>
2019-02-28 13:52:22 +0300

    fix(api-client): handle empty workspace

--- packages/api-client/src/importWorkspace.js
+++ packages/api-client/src/importWorkspace.js
@@ -1,10 +1,11 @@
 // @flow
 import type { DocumentNode } from 'graphql';
+import * as R from 'ramda';
 
 import { importData } from './importData';
 import { importTables } from './importTables';
 
 export const importWorkspace = async (request: (query: string | DocumentNode, variables?: Object) => Promise<Object>, workspace: Object) => {
-  await importTables(request, workspace.tables);
-  await importData(request, workspace.data);
+  await importTables(request, R.propOr({}, 'tables', workspace));
+  await importData(request, R.propOr({}, 'data', workspace));
 };
