ac3ef8801c50163eb3037d8a63dc3a18901001e5
ej9x <strcmp9@gmail.com>
2019-03-01 18:25:22 +0300

    fix(generators): change prettier execution for webpack compability

--- packages/generators/src/formatCode.js
+++ packages/generators/src/formatCode.js
@@ -1,9 +1,11 @@
 // @flow
-import prettier from 'prettier';
+import prettier from 'prettier/standalone';
+import prettierBabylon from 'prettier/parser-babylon';
 
 export const formatCode = (code: string) => prettier.format(code, {
   printWidth: 120,
   singleQuote: true,
   trailingComma: 'es5',
   parser: 'babel',
+  plugins: [prettierBabylon],
 });
