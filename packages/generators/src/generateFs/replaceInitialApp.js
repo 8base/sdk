aa22ab4f99a4ec75dc5302e27a16160e747d1117
ej9x <strcmp9@gmail.com>
2019-04-01 17:57:32 +0300

    fix(generators): add support for api-token generate app

--- packages/generators/src/generateFs/replaceInitialApp.js
+++ packages/generators/src/generateFs/replaceInitialApp.js
@@ -14,9 +14,10 @@ type AppFs = {
 }
 
 type AppFsConstants = {
-  endpoint: string,
-  authClientId: string,
-  authDomain: string,
+  endpoint?: string,
+  authClientId?: string,
+  authDomain?: string,
+  apiToken?: string,
   appName: string,
 }
 
@@ -31,25 +32,33 @@ const removePieceOfCode = (code: string, pieceName: string) => {
 };
 
 
-export const replaceInitialApp = (fsObject: AppFs, constants: AppFsConstants): AppFs => {
+export const replaceInitialApp = (fsObject: AppFs, constants: AppFsConstants, config: { authMode: 'web' | 'api-token' }): AppFs => {
   const fsObjectReplaced: AppFs = {};
+  const isApiTokenMode = config.authMode === 'api-token';
+
 
   Object.keys(fsObject).forEach(filePath => {
     switch (filePath) {
       case FS_NAMES.APPLICATION_JS: {
-        fsObjectReplaced[FS_NAMES.APPLICATION_JS] = fsObject[FS_NAMES.APPLICATION_JS] && removePieceOfCode(fsObject[FS_NAMES.APPLICATION_JS], 'AUTH_API_TOKEN')
-          .replace('__APP_API_ENDPOINT__', constants.endpoint)
-          .replace('__APP_AUTH_CLIENT_ID__', constants.authClientId)
-          .replace('__APP_AUTH_DOMAIN__', constants.authDomain)
-          .replace('auth0WebClient', 'authClient')
-          .replace('import { ApiTokenAuthClient } from \'@8base/api-token-auth-client\';\n', '');
+        fsObjectReplaced[FS_NAMES.APPLICATION_JS] = isApiTokenMode
+          ? fsObject[FS_NAMES.APPLICATION_JS] && removePieceOfCode(fsObject[FS_NAMES.APPLICATION_JS], 'AUTH_WEB')
+            .replace('__APP_API_TOKEN__', constants.apiToken || '')
+            .replace('__APP_API_ENDPOINT__', constants.endpoint || '')
+            .replace('import { WebAuth0AuthClient } from \'@8base/web-auth0-auth-client\';\n', '')
+
+          : fsObject[FS_NAMES.APPLICATION_JS] && removePieceOfCode(fsObject[FS_NAMES.APPLICATION_JS], 'AUTH_API_TOKEN')
+            .replace('__APP_API_ENDPOINT__', constants.endpoint || '')
+            .replace('__APP_AUTH_CLIENT_ID__', constants.authClientId || '')
+            .replace('__APP_AUTH_DOMAIN__', constants.authDomain || '')
+            .replace('auth0WebClient', 'authClient')
+            .replace('import { ApiTokenAuthClient } from \'@8base/api-token-auth-client\';\n', '');
 
         break;
       }
 
       case FS_NAMES.APPOLO_CONFIG_JS: {
         fsObjectReplaced[FS_NAMES.APPOLO_CONFIG_JS] = fsObject[FS_NAMES.APPOLO_CONFIG_JS] && fsObject[FS_NAMES.APPOLO_CONFIG_JS]
-          .replace('__APP_API_ENDPOINT__', constants.endpoint);
+          .replace('__APP_API_ENDPOINT__', constants.endpoint || '');
 
         break;
       }
