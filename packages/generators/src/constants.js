337d4878961780dc0c3b558065c81ba0f6621fcc
ej9x <strcmp9@gmail.com>
2019-05-13 20:21:13 +0300

    build: remove generators package from sdk

--- packages/generators/src/constants.js
+++
@@ -1,4 +0,0 @@
-// @flow
-
-/** Name of branch in the repo https://github.com/8base/react-app-starter */
-export const REPO_BRANCH_NAME = 'v0.3.2';
--- packages/generators/src/generateFs/generateFileNames.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import changeCase from 'change-case';
-
-export const getCreateFormFileName = (screenName: string) => `${changeCase.pascal(screenName)}CreateDialog.js`;
-export const getEditFormFileName = (screenName: string) => `${changeCase.pascal(screenName)}EditDialog.js`;
-export const getDeleteFormFileName = (screenName: string) => `${changeCase.pascal(screenName)}DeleteDialog.js`;
-export const getTableFileName = (screenName: string) => `${changeCase.pascal(screenName)}Table.js`;
-export const getScreenFolderName = (screenName: string) => changeCase.camel(screenName);
