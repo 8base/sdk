c4e220ce9134485f4ff14d2cc94cc2df9d94677c
Alexey Ryabov <lesha_12_01@mail.ru>
2019-06-17 15:27:56 +0300

    chore(api-client): importWorkspace small fix

--- packages/api-client/src/importWorkspace.ts
+++ packages/api-client/src/importWorkspace.ts
@@ -5,7 +5,7 @@ import { importData } from './importData';
 import { importTables } from './importTables';
 
 export const importWorkspace = async (
-  request: <T extends object>(query: string | DocumentNode, variables?: object) => Promise<T>,
+  request: <T extends any>(query: string | DocumentNode, variables?: object) => Promise<T>,
   workspace: object,
 ) => {
   await importTables(request, R.propOr({}, 'tables', workspace));
